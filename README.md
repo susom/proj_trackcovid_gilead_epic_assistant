e18c3c4d1a578f5e482c9c582b2353e28fb4489f
Andrew Martin <andy123@stanford.edu>
2020-09-21 19:32:53 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TrackCovid GenPop EPIC Assistant
+
+This EM updates a few fields in the project with the proper format for being exported into EPIC.  It also keeps those fields up-to-date when the records are re-saved.a
+
+It only monitors the baseline_arm_1 event and is specialized to the GenPop Main Project
---
+++ TrackCovidGileadEpicAssistant.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace Stanford\TrackCovidGileadAssistant;
+
+require_once "emLoggerTrait.php";
+
+use \REDCap;
+
+class TrackCovidGileadAssistant extends \ExternalModules\AbstractExternalModule
+{
+
+    use emLoggerTrait;
+
+    CONST EM_EVENT_NAME = "screening_arm_1";
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // Other code to run when object is instantiated
+    }
+
+    public function redcap_save_record($project_id, $record, $instrument, $event_id, $group_id, $survey_hash = NULL, $response_id = NULL, $repeat_instance = 1) {
+
+        $em_event_id = REDCap::getEventIdFromUniqueEvent($this::EM_EVENT_NAME);
+
+        // $this->emDebug(".".$event_id.".", ".".$em_event_id.".");
+
+        if ($event_id != $em_event_id) {
+            // This is the wrong event
+            // $this->emDebug("Wrong Event");
+            return;
+        }
+        $this->emDebug("Updating $record");
+
+        $records = $this->getRecordData(array($record));
+        $updates = $this->parseRecords($records);
+        $q = $this->updateRecords($updates);
+
+        if (!empty($q['errors'])) {
+            REDCap::logEvent("Error updating in " . $this->getModuleName(), json_encode($updates),"",$record, $event_id);
+        }
+
+    }
+
+
+
+    public function getRecordData($records = null) {
+        $params = [
+            "project_id"    => $this->getProjectId(),
+            "records"       => $records,
+            "return_format" => "json",
+            "events"        => [ self::EM_EVENT_NAME ],
+        ];
+
+        $q = REDCap::getData($params);
+        return json_decode($q,true);
+    }
+
+
+    /**
+     * Parse through the records to return an array of updates
+     * @param $data array  A json-exported redcap array of records
+     * @return array
+     */
+    public function parseRecords($results) {
+
+        $updates = [];
+
+        $force = $this->getProjectSetting('force-update');
+
+        foreach ($results as $r) {
+
+            list($city, $state, $zip) = $this->parseCsz($r["csz"]);
+            $address   = $this->parseAddr($r["addr"]);
+            $lang      = $this->parseLang($r['confirm_language']);
+            $ethnicity = $this->parseEthnicity($r);
+            $sex       = $this->parseSex($r);
+
+            $update = [];
+
+            // Update primary_city from CSZ
+            if (( $force || empty($r['primary_city']))           && !empty($city)) $update['primary_city'] = $city;
+
+            // Update State from CSZ
+            if (( $force || empty($r['primary_state']))          && !empty($state)) $update['primary_state'] = $state;
+
+            // Update language
+            if (( $force || empty($r['stanford_epic_lang']))     && !empty($lang)) $update['stanford_epic_lang'] = $lang;
+
+            // Update ethnicity
+            if (( $force || empty($r['stanford_epic_ethnicity']))&& !empty($ethnicity)) $update['stanford_epic_ethnicity'] = $ethnicity;
+
+            // Update ethnicity
+            if (( $force || empty($r['stanford_epic_sex']))      && !empty($sex)) $update['stanford_epic_sex'] = $sex;
+
+
+            if (!empty($update)) {
+                $update[REDCap::getRecordIdField()] = $r[REDCap::getRecordIdField()];
+                $update['redcap_event_name']        = $this::EM_EVENT_NAME;
+
+                $updates[] = $update;
+            }
+        }
+
+        return $updates;
+    }
+
+    public function updateRecords($updates) {
+        $this->emDebug('updates',$updates);
+        $q = REDCap::saveData($this->getProjectId(), 'json', json_encode($updates));
+        //$this->emDebug($q);
+        return $q;
+    }
+
+    public function parseSex($row) {
+        $saad = $row["saab"];
+        switch ($saad) {
+            case "1":
+                $sex = "M";
+                break;
+            case "2":
+                $sex = "F";
+                break;
+            default:
+                $sex = "U";
+                break;
+        }
+        return $sex;
+    }
+
+    public function parseCsz($csz)
+    {
+        global $module;
+
+        $re      = '/^([^,]+)\,\s+(\w+)\s+(\d{5})/';
+        $matches = [];
+        $count   = preg_match_all($re, $csz, $matches);
+
+        $city  = isset($matches[1][0]) ? trim($matches[1][0]) : "";
+        $state = isset($matches[2][0]) ? trim($matches[2][0]) : "";
+        $zip   = isset($matches[3][0]) ? trim($matches[3][0]) : "";
+
+        return array($city, $state, $zip);
+    }
+
+    public function parseAddr($addr)
+    {
+        global $module;
+        // $module->emDebug($addr);
+
+        return trim($addr);
+    }
+
+    public function parseLang($lang)
+    {
+        global $module;
+        // $module->emDebug($lang);
+
+        switch ($lang) {
+            case "1":
+                $result = "ENG";
+                break;
+            case "2":
+                $result = "SPA";
+                break;
+            case "3":
+                $result = "MDN";
+                break;
+            case "4":
+                $result = "TGL";
+                break;
+            case "5":
+                $result = "VIE";
+                break;
+            default:
+                $result = "";
+        }
+
+        return trim($result);
+
+    }
+
+    public function parseEthnicity($row) {
+            // Figure out ethnicity
+        $latino_origin = $row['latino_origin'];
+        switch ($latino_origin) {
+            case "1":
+                $ethnicity = "Hispanic/Latino";
+                break;
+            case "0":
+                $ethnicity = "Non-Hispanic/Non-Latino";
+                break;
+            default:
+                $ethnicity = "Unknown";
+        }
+        return trim($ethnicity);
+    }
+
+
+}
